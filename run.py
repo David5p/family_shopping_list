42ecfa09577790c1176ac6bd8b29f37f7d506e0f
David5p <k9glpuwd@students.codeinstitute.net>
2025-07-28 08:56:44 +0200

    Add start message function

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message():
+    print("Welcome to the Family Shopping List")
+
+welcome_message()
